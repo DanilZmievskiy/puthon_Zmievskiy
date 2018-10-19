0d0a6ef1b1947ad3ba395654eb42bf0cb33437d6
DanilZmievskiy <danilzmievskiy@gmail.com>
2018-10-19 20:31:36 +0300

    Реализована функция encrypt_vigenere()

--- homework01/vigener.py
+++ homework01/vigener.py
@@ -9,7 +9,36 @@ def encrypt_vigenere(plaintext, keyword):
     >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
     'LXFOPVEFRNHR'
     """
-    # PUT YOUR CODE HERE
+    j = 0
+    ciphertext = ""
+    for i in plaintext:
+        if ord('a') <= ord(keyword[j]) <= ord('z'):
+            numb = ord(keyword[j]) - 122 + 25
+        elif ord('A') <= ord(keyword[j]) <= ord('Z'):
+            numb = ord(keyword[j]) - 90 + 25
+        i = ord(i)
+        if (ord('a') <= i <= ord('z')):
+            i += numb
+            if i > 122:
+                i -= 122
+                a = 96 + i
+                a = chr(a)
+            else:
+                a = chr(i)
+        elif (ord('A') <= i <= ord('Z')):
+            i += numb
+            if i > 90:
+                i -= 90
+                a = 64 + i
+                a = chr(a)
+            else:
+                a = chr(i)
+        else:
+            a = chr(i)
+        ciphertext += a
+        j += 1
+        if j > len(keyword) - 1:
+            j = 0
     return ciphertext
 
 
