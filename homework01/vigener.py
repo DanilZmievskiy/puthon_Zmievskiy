73c5ad467cc82581d341ac52176dde404f8bab41
DanilZmievskiy <44033128+DanilZmievskiy@users.noreply.github.com>
2018-10-22 20:04:59 +0300

    Update vigener.py

--- homework01/vigener.py
+++ homework01/vigener.py
@@ -9,7 +9,28 @@ def encrypt_vigenere(plaintext, keyword):
     >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
     'LXFOPVEFRNHR'
     """
-    # PUT YOUR CODE HERE
+    ciphertext = ""
+    j = 0
+    for i in plaintext:
+        if ord('a') <= ord(keyword[j]) <= ord('z'):
+            numb = ord(keyword[j]) - 122 + 25
+        elif ord('A') <= ord(keyword[j]) <= ord('Z'):
+            numb = ord(keyword[j]) - 90 + 25
+        i = ord(i)
+        if ord('a') <= i <= ord('z'):
+                i += numb
+                if i > 122:
+                    i -= 122
+                    i = 96 + i
+        elif ord('A') <= i <= ord('Z'):
+            i += numb
+            if i > 90:
+                i -= 90
+                i = 64 + i
+        ciphertext += chr(i)
+        j += 1
+        if j > len(keyword) - 1:
+            j = 0
     return ciphertext
 
 
@@ -24,5 +45,26 @@ def decrypt_vigenere(ciphertext, keyword):
     >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
     'ATTACKATDAWN'
     """
-    # PUT YOUR CODE HERE
+    plaintext = ""
+    j = 0
+    for i in ciphertext:
+        if ord('a') <= ord(keyword[j]) <= ord('z'):
+            numb = ord(keyword[j]) - 122 + 25
+        elif ord('A') <= ord(keyword[j]) <= ord('Z'):
+            numb = ord(keyword[j]) - 90 + 25
+        i = ord(i)
+        if ord('a') <= i <= ord('z'):
+                i -= numb
+                if i < 97:
+                    i -= 97
+                    i = 123 + i
+        elif ord('A') <= i <= ord('Z'):
+            i -= numb
+            if i < 65:
+                i -= 65
+                i = 91 + i
+        plaintext += chr(i)
+        j += 1
+        if j > len(keyword) - 1:
+            j = 0
     return plaintext
